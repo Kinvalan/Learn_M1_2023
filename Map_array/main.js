5d86e5da0569a29a9b0abc67afda83b7dbc37866
Kinvalan <sebastianmoller01@icloud.com>
2023-02-17 14:06:38 +0100

    Updated comments

--- Map_array/main.js
+++ Map_array/main.js
@@ -14,7 +14,7 @@ let newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 updateView()
 function updateView() {
     let html = /*HTML*/ ` 
-    <input type="text" onchange="findArrayNumbers(this.value)">
+    <input type="number" onchange="findArrayNumbers(this.value)">
     `;
     document.getElementById('app').innerHTML = html;
 }
@@ -24,26 +24,16 @@ function updateView() {
 // Controller
 
 /* 
-Programmet antar at det som er etter pila i newArray.map() / arrow-funskjonen under skal returneres.
+.map() kaller en gitt callback funksjon for hver gjenstand i arrayet i rekkefølge 
+- og returnerer et array med resultatet.
 
+Programmet antar at det som er etter pila ved newArray.map() skal returneres.
 Funksjonen findArrayNumbers tar nå det tallet du skriver inn og ganger det i 10-gangen (fordi arrayet går fra 1 til 10).
-
-Skriver vi => index == number og un-commenter if else-ene, vil koden sjekke om det vi skreiv i inputet er i arrayet.
-Den vil samtidig si med tekst om arrayet inneholder det vi tallet vi skreiv i inputet eller ikke.
 */ 
 
 function findArrayNumbers(number) {
-    let searchArray = newArray.map(index => index * number)
-
-    // if (searchArray) {
-    //     console.log("Arrayet inneholder " + number);
-    // }
-
-    // else {
-    //     console.log("Fant ikke " + number + " i arrayet");
-    // }
-
-    console.log(searchArray);
+    let searchArray = newArray.map(index => index * number) 
+    console.log(searchArray);                                    
     updateView();
 }
 
